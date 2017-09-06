f48a7faaaa550b402c5cac246ab28086c9741950
Alexander Shypovskikh <core.editor>
2017-09-06 22:31:31 +0400

    master

--- src/main/java/com/shypovskikh/controllers/LoginController.java
+++ src/main/java/com/shypovskikh/controllers/LoginController.java
@@ -1,16 +1,21 @@
-package com.shypovskikh.springforms;
+package com.shypovskikh.controllers;
 
 
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shypovskikh.models.User;
@@ -34,5 +39,36 @@ public class LoginController {
 		return "main";
 	}
 	
+	@RequestMapping(value="/get-json-user", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public User getJsonUser(@RequestParam("name") String name) {
+		User user = new User();
+		user.setEmail(name);
+		return user;
+	}
+	
+	@RequestMapping(value="/put-json-user", method = RequestMethod.POST, consumes = "application/json")
+	public ResponseEntity<String> setJsonUser(@RequestBody User user){
+		logger.info(user.getEmail());
+		return new ResponseEntity<String>(HttpStatus.ACCEPTED);
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
